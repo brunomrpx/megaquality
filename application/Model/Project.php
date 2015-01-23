8bb7bfab96ebf9fe6b7918ea07e5dace82f049b0
Brunomrpx <brunomrpx@gmail.com>
2015-01-23 18:03:04 -0200

    Implementacao para os projetos serem buscados de uma API externa.

--- application/Model/Project.php
+++ application/Model/Project.php
@@ -1,26 +1,23 @@
 <?php
 
 App::uses('AppModel', 'Model');
+App::uses('HttpSocket', 'Network/Http');
 
 class Project extends AppModel {
-    public $useTable = false;
-    public $projects = array(
-        0 => array(
-            'id' => 1,
-            'name' => 'Projeto 1',
-            'team' => 'Equipe 1',
-            'status' => 'Em andamento'
-        ),
-        1 => array(
-            'id' => 2,
-            'name' => 'Projeto 2',
-            'team' => 'Equipe 2',
-            'status' => 'Em andamento'
-        ),
-    );
-    public function getAll() {
-        return $this->projects; 
+	
+    public $useTable = false;    
+    
+    public function getAll($cookies = null) {
+    	$httpSocket = new HttpSocket();    	
+    	$httpSocketResponse = $httpSocket->get(
+    			'url',
+    			array(),
+    			array('cookies' => $cookies)
+    	);
+    	
+    	return json_decode($httpSocketResponse->body())->projects;    	
     }
+    
     public function getById($id = null) {
         if (!is_null($id)) {
             foreach ($this->projects as $project) {
